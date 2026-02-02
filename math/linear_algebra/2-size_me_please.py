d01e678fb1528bc9df692017c597ba11fe0ce044
OgayoTK1 <a.ogayo@alustudent.com>
2026-02-02 16:39:08 +0200

    defining a function

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,7 @@
+def matrix_shape(matrix):
+    shape = []
+    current = matrix
+    while isinstance(current, list) and current:
+        shape.append(len(current))
+        current = current[0]
+    return shape
