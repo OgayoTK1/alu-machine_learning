902ebdb5ae13e853964feed9511707ec08f398e9
OgayoTK1 <a.ogayo@alustudent.com>
2026-02-02 10:30:24 +0200

    line graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-# your code here
+# Plot y versus its index values
 plt.plot(y, 'r-')        # 'r' = red, '-' = solid line
 plt.xlim(0, 10)          # forces x-axis to range exactly from 0 to 10
 plt.show()
