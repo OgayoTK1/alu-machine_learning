c022b1a055c66a0447fb585e8dd38b3849a1d60f
OgayoTK1 <a.ogayo@alustudent.com>
2026-02-03 09:00:08 +0200

    flipping over

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""Matrix transpose module"""
+
+
 def matrix_transpose(matrix):
+    """Returns the transpose of a 2D matrix"""
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
 
